3ae61c676e54f45563ec4e8f0e63a56c808abf7e
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-08-17 14:51:30 +0200

    setup dependencies and navigation

--- Android/app/src/main/java/com/example/serversmanagement/MainActivity.kt
+++ Android/app/src/main/java/com/example/serversmanagement/MainActivity.kt
@@ -3,36 +3,19 @@ package com.example.serversmanagement
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.serversmanagement.navigation.Navigation
 import com.example.serversmanagement.ui.theme.ServersManagementTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ServersManagementTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+                Navigation()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ServersManagementTheme {
-        Greeting("Android")
-    }
 }
---
+++ Android/app/src/main/java/com/example/serversmanagement/navigation/Navigation.kt
@@ -0,0 +1,63 @@
+package com.example.serversmanagement.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "instances_list_screen")
+    {
+        composable("instances_list_screen") {
+
+        }
+        composable("instance_details_screen/{name}/{ipaddress}/{status}/{memory}/{type}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+
+                },
+                navArgument("ipaddress") {
+                    type = NavType.StringType
+                },
+                navArgument("status") {
+                    type = NavType.BoolType
+                },
+                navArgument("memory") {
+                    type = NavType.IntType
+                },
+                navArgument("type") {
+                    type = NavType.StringType
+                }
+            )) {
+            val instanceName = remember {
+                val name = it.arguments?.getString("name")
+                name?.let { it } ?: "Linux Ubuntu"
+            }
+            val instanceIpAddress = remember {
+                val ipaddress = it.arguments?.getString("ipaddress")
+                ipaddress?.let { it } ?: "127.0.0.1"
+            }
+            val instanceStatus = remember {
+                val status= it.arguments?.getBoolean("status")
+                status?.let { it } ?: false
+            }
+            val instanceMemory = remember {
+                val memory = it.arguments?.getInt("memory")
+                memory?.let { it } ?: 1
+            }
+            val instanceType = remember {
+                val type = it.arguments?.getString("type")
+                type?.let { it } ?: "Web Server"
+            }
+
+        }
+    }
+}
